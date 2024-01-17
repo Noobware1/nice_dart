36f38bcadf5d120ef31feab4664257e22759b07c
Noobware1 <110553648+Noobware1@users.noreply.github.com>
2024-01-17 22:47:50 +0530

    fixed exports

--- lib/dartx.dart
+++ lib/dartx.dart
@@ -4,10 +4,11 @@ export 'src/extensions/string.dart';
 export 'src/extensions/list.dart';
 export 'src/extensions/iterable.dart';
 export 'src/extensions/scopes.dart';
+export 'src/extensions/map.dart';
+export 'src/extensions/object.dart';
 export 'src/builders.dart';
 export 'src/functions.dart';
-
-// export 'src/extensions/map.dart';
+export 'src/result.dart';
 // export 'src/extensions/num.dart';
 // export 'src/extensions/set.dart';
 
